6ac251d37a4ecca5736e980685334001c4554726
Rubén <hey@rubenvara.io>
2023-04-11 03:59:31 +0200

    try to create months and days

---
+++ src/Month.tsx
@@ -0,0 +1,53 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { styled } from 'solid-styled-components';
+import { currentYear } from './config';
+
+dayjs.extend(customParseFormat);
+
+const StyledMonth = styled.article`
+  border: 1px solid;
+  padding: 12px;
+  table {
+    border-collapse: collapse;
+    width: 100%;
+    td {
+      border: 1px solid;
+    }
+  }
+`;
+
+interface IMonth {
+  month: string;
+  index: number;
+}
+
+export function Month(props: IMonth) {
+  const first = dayjs(
+    `1-${props.index + 1}-${currentYear}-12`,
+    'D-M-YYYY-H',
+    'es'
+  );
+  const firstDayOfWeek = first.day();
+
+  return (
+    <StyledMonth>
+      <h3>
+        {props.index}. {props.month}
+      </h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>{firstDayOfWeek === 1 && 'start'}</td>
+            <td>{firstDayOfWeek === 2 && 'start'}</td>
+            <td>{firstDayOfWeek === 3 && 'start'}</td>
+            <td>{firstDayOfWeek === 4 && 'start'}</td>
+            <td>{firstDayOfWeek === 5 && 'start'}</td>
+            <td>{firstDayOfWeek === 6 && 'start'}</td>
+            <td>{firstDayOfWeek === 0 && 'start'}</td>
+          </tr>
+        </tbody>
+      </table>
+    </StyledMonth>
+  );
+}

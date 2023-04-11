ae44161674b59de9599347f571e059d7c2a6c097
Rubén <hey@rubenvara.io>
2023-04-11 13:11:43 +0200

    vite path alises

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      $components: path.resolve(__dirname, './src/components'),
+      $src: path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
   },

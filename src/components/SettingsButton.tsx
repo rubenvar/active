ed055f7311dfd04e9561f22b27f8912f384bbfa4
Rubén <hey@rubenvara.io>
2023-05-15 16:49:02 +0200

    settings popup default open if no activites created yet

--- src/components/SettingsButton.tsx
+++ src/components/SettingsButton.tsx
@@ -1,10 +1,17 @@
 import { IconAdjustments } from '@tabler/icons-solidjs';
-import { Show, createSignal } from 'solid-js';
+import { Show, createSignal, onMount } from 'solid-js';
 import { Portal } from 'solid-js/web';
 import { SettingsPopup } from './SettingsPopup';
+import { useActivities } from '$src/stores/ActivityContext';
 
 export function SettingsButton() {
   const [isOpen, setIsOpen] = createSignal(false);
+  const [activities] = useActivities();
+
+  // start app with settings popup open by default if no activities list in localStorage
+  onMount(() => {
+    if (!activities.length) setIsOpen(true);
+  });
 
   return (
     <>
